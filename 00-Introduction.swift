c0916c7f3a3df00971fdea93d2b7540b4819301c
7ni97 <75171008+7ni97@users.noreply.github.com>
2020-11-28 16:32:20 +0200

    Create 00-Introduction.swift

---
+++ 00-Introduction.swift
@@ -0,0 +1,17 @@
+/// The text in this repository copies freely from the documentation.
+/// Generally, these should be free standing Swift files which you can
+/// directly execute from the command line:
+///
+///     $ swift 01-Cancellable.swift
+///
+/// However, you might find it more readable to paste them in an Xcode
+/// Playground and execute them there (`⌘ + ⇧ + return`).
+
+import Combine
+
+/// Click on the import Combine above. The only import you'll see there is
+///
+///     import Darwin
+///
+/// Combine is closed source. But it has no other dependencies except the
+/// platform itself.
